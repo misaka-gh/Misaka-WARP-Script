91fc1921f8adeda4efd6f1444e19c94a59eb53ef
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-03-31 02:14:57 +0800

    Update uninstall.sh

--- wgcf-warp/uninstall.sh
+++ wgcf-warp/uninstall.sh
@@ -37,8 +37,9 @@ uninstall(){
     wg-quick down wgcf 2>/dev/null
     systemctl disable wg-quick@wgcf 2>/dev/null
     ${PACKAGE_UNINSTALL[int]} wireguard-tools wireguard-dkms 2>/dev/null
-    rm -f /usr/local/bin/wgcf 
+    [[ -z $(type -P wireproxy) ]] && rm -f /usr/local/bin/wgcf 
     rm -f /etc/wireguard/wgcf.conf
+    rm -f /etc/wireguard/wgcf-account.toml
     rm -f /usr/bin/wireguard-go
     if [[ -e /etc/gai.conf ]]; then
         sed -i '/^precedence[ ]*::ffff:0:0\/96[ ]*100/d' /etc/gai.conf
