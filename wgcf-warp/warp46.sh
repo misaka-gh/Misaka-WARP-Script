216c5ff911c166227a76c53b403b4a071fcfda3b
misakano7545 <misakano7545@protonmail.com>
2022-05-13 23:54:11 +0800

    Update script

--- wgcf-warp/warp46.sh
+++ wgcf-warp/warp46.sh
@@ -72,8 +72,8 @@ checkCentOS8(){
 install_wireguard_centos(){
     ${PACKAGE_INSTALL[int]} epel-release
     ${PACKAGE_INSTALL[int]} net-tools wireguard-tools iptables
-    if [ "$main" -lt 5 ] || [ "$minor" -lt 6 ]; then 
-        if [[ ${vpsvirt} == "kvm" || ${vpsvirt} == "xen" || ${vpsvirt} == "microsoft" ]]; then
+    if [[ $main -lt 5 ]] || [[ $minor -lt 6 ]]; then 
+        if [[ $vpsvirt =~ "kvm"|"xen"|"microsoft" ]]; then
             vsid=`grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1`
             curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-$vsid/jdoss-wireguard-epel-$vsid.repo
             ${PACKAGE_INSTALL[int]} wireguard-dkms
@@ -86,8 +86,8 @@ install_wireguard_debian(){
     echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | tee /etc/apt/sources.list.d/backports.list
     ${PACKAGE_UPDATE[int]}
     ${PACKAGE_INSTALL[int]} --no-install-recommends net-tools iproute2 openresolv dnsutils wireguard-tools iptables
-    if [ "$main" -lt 5 ] || [ "$minor" -lt 6 ]; then
-        if [[ ${vpsvirt} == "kvm" || ${vpsvirt} == "xen" || ${vpsvirt} == "microsoft" ]]; then
+    if [[ $main -lt 5 ]] || [[ $minor -lt 6 ]]; then
+        if [[ $vpsvirt =~ "kvm"|"xen"|"microsoft" ]]; then
             ${PACKAGE_INSTALL[int]} --no-install-recommends linux-headers-$(uname -r)
             ${PACKAGE_INSTALL[int]} --no-install-recommends install wireguard-dkms
         fi
