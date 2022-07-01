b66ffbe893ccd0548f2db7f90fb469d89fad52ce
misakano7545 <misakano7545@protonmail.com>
2022-07-02 00:54:22 +0800

    Optimize script

--- misakawarp.sh
+++ misakawarp.sh
@@ -842,21 +842,24 @@ warpup(){
     yellow "请按照下面指示，输入您的CloudFlare WARP账号信息："
     read -rp "请输入您的WARP设备ID (36位字符): " license
     read -rp "请输入你期望刷到的流量 (单位: GB): " flowdata
-    echo -e "已设置你期望刷到的流量为：$WarpFlowLimit GB"
-    for ((i = 0; i < ${flowdata}; i++)); do
-    [[ $i == 0 ]] && sleep_try=30 && sleep_min=20 && sleep_max=600 && echo $(date) Mission $flowdata GB
-    install_id=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 22) && \
-    curl -X POST -m 10 -sA "okhttp/3.12.1" -H 'content-type: application/json' -H 'Host: api.cloudflareclient.com' \
-    --data "{\"key\": \"$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 43)=\",\"install_id\": \"$install_id\",\"fcm_token\": \"APA91b$install_id$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 134)\",\"referrer\": \"$license\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(tr -dc '0-9' </dev/urandom | head -c 3)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}" \
-    --url "https://api.cloudflareclient.com/v0a$(shuf -i 100-999 -n 1)/reg" | grep -qE "referral_count\":1" && status=0 || status=1
-    # cloudflare限制了请求频率,目前测试大概在20秒,失败时因延长sleep时间
-    [[ $sleep_try > $sleep_max ]] && sleep_try=300
-    [[ $sleep_try == $sleep_min ]] && sleep_try=$((sleep_try+1))
-    [[ $status == 0 ]] && sleep_try=$((sleep_try-1)) && sleep $sleep_try && rit[i]=$i && echo -n $i-o- && continue
-    [[ $status == 1 ]] && sleep_try=$((sleep_try+2)) && sleep $sleep_try && bad[i]=$i && echo -n $i-x- && continue
-done
     echo ""
-    echo -e "本次运行共成功获取warp+流量 ${GREEN} ${#rit[*]} ${PLAIN} GB"
+    echo -e "已设置你期望刷到的WARP+流量为：$flowdata GB"
+    for ((i = 0; i < ${flowdata}; i++)); do
+        [[ $i == 0 ]] && sleep_try=30 && sleep_min=20 && sleep_max=600 && echo $(date) Mission $flowdata GB
+
+        install_id=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 22) && \
+        curl -X POST -m 10 -sA "okhttp/3.12.1" -H 'content-type: application/json' -H 'Host: api.cloudflareclient.com' \
+        --data "{\"key\": \"$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 43)=\",\"install_id\": \"$install_id\",\"fcm_token\": \"APA91b$install_id$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 134)\",\"referrer\": \"$license\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(tr -dc '0-9' </dev/urandom | head -c 3)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}" \
+        --url "https://api.cloudflareclient.com/v0a$(shuf -i 100-999 -n 1)/reg" | grep -qE "referral_count\":1" && status=0 || status=1
+
+        # cloudflare限制了请求频率,目前测试大概在20秒,失败时因延长sleep时间
+        [[ $sleep_try > $sleep_max ]] && sleep_try=300
+        [[ $sleep_try == $sleep_min ]] && sleep_try=$((sleep_try+1))
+        [[ $status == 0 ]] && sleep_try=$((sleep_try-1)) && sleep $sleep_try && rit[i]=$i && echo -n $i-o- && continue
+        [[ $status == 1 ]] && sleep_try=$((sleep_try+2)) && sleep $sleep_try && bad[i]=$i && echo -n $i-x- && continue
+    done
+    echo ""
+    echo -e "本次运行共成功获取WARP+流量 ${GREEN} ${#rit[*]} ${PLAIN} GB"
 }
 
 warpsw1_freeplus(){
@@ -1003,7 +1006,7 @@ warpsw1(){
                     warpsw1_freeplus
                     
                     wg-quick up wgcf >/dev/null 2>&1
-
+                    
                     red "WARP Teams配置有误，已自动降级至WARP 免费账户 / WARP+！"
                 fi
             done
